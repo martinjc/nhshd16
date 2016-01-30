6de489808e8aac8fb5511fb135dad2429285fd23
docjaq <jonaquinn@gmail.com>
2016-01-30 11:55:19 +0000

    Added basic active/inactive panel management.

---
+++ Assets/code/PanelManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PanelManager : MonoBehaviour
+{
+	public Transform PanelActive;
+	public Transform PanelInActive;
+	public Transform PanelMain;
+
+	static private PanelManager panelManager;
+	static public void Show (Transform panel)
+	{
+		if (panelManager == null)
+		{
+			GameObject gameObject = GameObject.Find ("Canvas");
+			if (gameObject != null)
+				panelManager = gameObject.GetComponent<PanelManager> ();
+		}
+
+		if (panelManager != null)
+		{
+			while (panelManager.PanelActive.childCount > 0)
+				panelManager.PanelActive.GetChild (0).transform.SetParent (panelManager.PanelInActive);
+
+			panel.SetParent (panelManager.PanelActive);
+		}
+	}
+
+	// Use this for initialization
+	void Start (){
+		Show (PanelMain);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+}
