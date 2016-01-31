7e6071afb4b05fc3af7437a2ef745b0f9693588d
Martin Chorley <m.j.chorley@cs.cardiff.ac.uk>
2016-01-31 09:48:33 +0000

    re-added dead and score

--- Assets/code/Data/GameState.cs
+++ Assets/code/Data/GameState.cs
@@ -7,9 +7,11 @@ public class GameState {
 	public int total_time;
 	public int dismissed;
 	public string state;
+	public int score
 	public int admitted;
 	public int time;
 	public int used_beds;
 	public int in_queue;
+	public int dead;
 	public int total_beds;
 }
