19bfd1bccd9592dd86bed668e0f1322fba3da789
Martin Chorley <m.j.chorley@cs.cardiff.ac.uk>
2016-01-31 09:48:56 +0000

    oops

--- Assets/code/Data/GameState.cs
+++ Assets/code/Data/GameState.cs
@@ -7,7 +7,7 @@ public class GameState {
 	public int total_time;
 	public int dismissed;
 	public string state;
-	public int score
+	public int score;
 	public int admitted;
 	public int time;
 	public int used_beds;
